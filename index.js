61333f7cc1e06f0159aea3379e29949a221858f0
ledsun <shigeru.nakajima@gmail.com>
2015-02-02 22:58:47 +0900

    Add alternative names of menus.

--- index.js
+++ index.js
@@ -7,7 +7,16 @@ var jsing = adventure('javascripting-jp');
 var problems = require('./menu.json');
 
 problems.forEach(function (problem) {
-  var p = problem.toLowerCase().replace(/\s/g, '-');
+  // eg. title(dir)
+  var link = problem.match(/.*\((\w[\s\w]*\w)\)/);
+
+  if(link){
+      var name = link[1];
+  }else{
+      var name = problem;
+  }
+
+  var p = name.toLowerCase().replace(/\s/g, '-');
   var dir = path.join(__dirname, 'problems', p);
   jsing.add(problem, function () { return require(dir); });
 });
